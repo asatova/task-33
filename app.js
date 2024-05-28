2791d9704f5775cc1e42deb78194d8cdb622f0a6
zarnigor <zarnigorasatova52@gmail.com>
2024-05-28 16:29:09 +0500

    feature

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const accordions = document.querySelectorAll(".accordion");
-const numAccordions = accordions.length;
 
-for (let i = 0; i < numAccordions; i++) {
-    accordions[i].addEventListener("click", function() {
+accordions.forEach((accordion) => {
+    accordion.addEventListener("click", function() {
         this.classList.toggle("active");
         const panel = this.nextElementSibling;
         panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + "px";
     });
-}
+});
